1f74fd7dd680f1f8989e5d797f50dd952c072d82
Anonymosotec <icotecc@gmail.com>
2022-10-25 15:43:10 -0500

    Interaccion entre todos los componentes

--- main.js
+++ main.js
@@ -5,18 +5,21 @@ const menu_mobile = document.querySelector('.mobile-menu');
 const navbar_shooping = document.querySelector('.navbar-shopping-cart');
 const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 const cards_container = document.querySelector('.cards-container');
-
+const productDetailContainer = document.querySelector('#productDetail');
+const productDetailCloseIcon = document.querySelector('.product-detail-close');
 
 navbar_email.addEventListener('click', function(event){
     event.preventDefault();
     desktop_menu.classList.toggle('inactive');
     shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 });
 
 menu_burger.addEventListener('click', function(event){
     event.preventDefault();
     menu_mobile.classList.toggle('inactive');
     shoppingCartContainer.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
 });
 
 navbar_shooping.addEventListener('click', function(event){
@@ -24,6 +27,18 @@ navbar_shooping.addEventListener('click', function(event){
     shoppingCartContainer.classList.toggle('inactive');
     menu_mobile.classList.add('inactive');
     desktop_menu.classList.add('inactive');
+    productDetailContainer.classList.add('inactive');
+});
+
+function openProductDetailAside(){
+    productDetailContainer.classList.remove('inactive');
+    menu_mobile.classList.add('inactive');
+    desktop_menu.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
+}
+
+productDetailCloseIcon.addEventListener('click', function(){
+    productDetailContainer.classList.add('inactive');
 });
 
 const product_list = [];
@@ -33,7 +48,7 @@ product_list.push({
     image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
 });
 product_list.push({
-    name: 'Kit de teclado y mouse Logitech MK120 Español Latinoamérica de color negro',
+    name: 'Kit de teclado y mouse Logitech MK120',
     prize: 70.991,
     image: 'https://http2.mlstatic.com/D_NQ_NP_643055-MLA48377746812_112021-V.webp'
 });
@@ -43,7 +58,7 @@ product_list.push({
     image: 'https://http2.mlstatic.com/D_NQ_NP_612278-MLA48623757086_122021-V.webp'
 });
 product_list.push({
-    name: 'Disco solido SSD interno Westen Digital WD Green',
+    name: 'Disco solido SSD',
     prize: 384.400,
     image: 'https://http2.mlstatic.com/D_NQ_NP_646272-MLA40283983443_122019-V.webp'
 });
@@ -53,7 +68,7 @@ product_list.push({
     image:'https://http2.mlstatic.com/D_Q_NP_909032-MLA47920360780_102021-P.webp'
 });
 product_list.push({
-    name: 'Kit 4 Botellas Gt35 Negra Y Gt52 Original',
+    name: 'Kit 4 Botellas Gt35',
     prize: 167.800,
     image:'https://http2.mlstatic.com/D_NQ_NP_813950-MCO41958233716_052020-W.webp'
 });
@@ -65,6 +80,8 @@ function renderProduct(array){
     
         const img = document.createElement('img');
         img.setAttribute('src', product.image);
+        img.setAttribute('loading', 'lazy');
+        img.addEventListener('dblclick', openProductDetailAside);
     
         const product_info = document.createElement('div');
         product_info.classList.add('product-info');
